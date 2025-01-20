95005129ae5bd07f7965fe31d4644d4d96f7ca02
Israel Oladeji <chengadc@gmail.com>
2025-01-20 00:38:47 -0500

    Update DirectionsController.php

--- app/Http/Controllers/DirectionsController.php
+++ app/Http/Controllers/DirectionsController.php
@@ -1,1 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class DirectionsController extends Controller
+{
+    public function index()
+    {
+        return view('directions')->with('pageTitle', 'Find Us');
+    }
+}
 
