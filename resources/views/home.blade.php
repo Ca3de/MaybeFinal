66fbd9f5be4d9b0f03fa6da1ab1fc7139d27d827
Israel Oladeji <chengadc@gmail.com>
2025-01-20 00:40:15 -0500

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,1 +1,9 @@
+@extends('layout.app')
+
+@section('content')
+    <section class="hero">
+        <h1>Welcome to Our Church</h1>
+        <p>Join us for worship, community, and spiritual growth.</p>
+    </section>
+@endsection
 
