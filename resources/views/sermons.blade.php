0ef089a6d8d18d20bd451ec1ea5958c4f6e2a06f
Israel Oladeji <chengadc@gmail.com>
2025-01-20 00:40:28 -0500

    Update sermons.blade.php

--- resources/views/sermons.blade.php
+++ resources/views/sermons.blade.php
@@ -1,1 +1,17 @@
+@extends('layout.app')
+
+@section('content')
+<div class="container">
+    <h2>Sermons</h2>
+    @foreach($sermons as $sermon)
+        <div class="sermon">
+            <h3>{{ $sermon->title }}</h3>
+            <iframe width="560" height="315" 
+                src="https://www.youtube.com/embed/{{ $sermon->youtube_video_id }}" 
+                frameborder="0" allowfullscreen>
+            </iframe>
+        </div>
+    @endforeach
+</div>
+@endsection
 
