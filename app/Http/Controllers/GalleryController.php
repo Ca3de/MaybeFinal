2f7341a8df8a811ce946968e9fef93017a724e6a
Israel Oladeji <chengadc@gmail.com>
2025-01-20 00:38:33 -0500

    Update GalleryController.php

--- app/Http/Controllers/GalleryController.php
+++ app/Http/Controllers/GalleryController.php
@@ -1,1 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Image;
+
+class GalleryController extends Controller
+{
+    public function index()
+    {
+        $images = Image::all(); // Get all images from database
+        return view('gallery', [
+            'images' => $images,
+            'pageTitle' => 'Church Gallery'
+        ]);
+    }
+}
 
