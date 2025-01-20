909892d688c6c0b8cbb628ab58b92678a6423fbd
Israel Oladeji <chengadc@gmail.com>
2025-01-20 00:40:40 -0500

    Update gallery.blade.php

--- resources/views/gallery.blade.php
+++ resources/views/gallery.blade.php
@@ -1,1 +1,16 @@
+@extends('layout.app')
+
+@section('content')
+<div class="container">
+    <h2>Gallery</h2>
+    <div class="gallery">
+        @foreach($images as $image)
+            <div class="gallery-item">
+                <img src="{{ asset('storage/'.$image->filename) }}" alt="{{ $image->title }}">
+                <p>{{ $image->title }}</p>
+            </div>
+        @endforeach
+    </div>
+</div>
+@endsection
 
