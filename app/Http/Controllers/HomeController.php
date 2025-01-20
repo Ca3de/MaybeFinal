22e4105f6ade52be40087b04341c0b2a7ae1cdd2
Israel Oladeji <chengadc@gmail.com>
2025-01-20 00:37:31 -0500

    Update HomeController.php

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -1,1 +1,15 @@
+<?php
 
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    public function index()
+    {
+        // SEO meta tags can be set dynamically if using a package or a custom solution
+        // e.g. with the Meta facade or manually in the view
+        return view('home')->with('pageTitle', 'Welcome to Our Church');
+    }
+}
