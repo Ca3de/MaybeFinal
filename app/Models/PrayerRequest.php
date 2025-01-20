8567550f6129d925cf8e8df66c9a496fccebbfe2
Israel Oladeji <chengadc@gmail.com>
2025-01-20 00:37:11 -0500

    Update PrayerRequest.php

--- app/Models/PrayerRequest.php
+++ app/Models/PrayerRequest.php
@@ -1,1 +1,13 @@
 
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class PrayerRequest extends Model
+{
+    protected $fillable = [
+        'name', 'email', 'request'
+    ];
+}
